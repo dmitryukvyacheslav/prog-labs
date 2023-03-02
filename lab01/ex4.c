7aadd026fa01d49f2553f14390344646547094c8
vyacheslavdmitryuk <dmitryuk.slavik@yandex.ru>
2023-03-03 02:21:29 +0700

    forgot the commentsgit add ex*.c git add ex*.c git add ex*.c !

--- lab01/ex4.c
+++ lab01/ex4.c
@@ -9,6 +9,8 @@ void bubble_sort(int* array, int size)
 {
     int i, j;
         for (i = 0; i < size - 1; ++i) {
+            /*  Исправлена ошибка - взятие лишнего элемента с конца
+                Было: (j = 0; j < size - i ; ++j) */
             for (j = 0; j < size - i - 1 ; ++j) {
                 if (array[j] > array[j + 1]) {
                     swap(&array[j], &array[j + 1]);
