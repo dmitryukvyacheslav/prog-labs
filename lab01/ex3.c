be48d9e681a146c1dbfbf826286517374f8dd5a4
Дмитрюк Вячеслав Вячеславович <is241s06@bwp15.csc.local>
2023-03-03 12:17:57 +0700

    encapsulate x*x in brackets

--- lab01/ex3.c
+++ lab01/ex3.c
@@ -3,11 +3,11 @@
     после развёртки макроса
     Было: #define SQR(x) x * x
 */
-#define SQR(x) (x) * (x)
+#define SQR(x) ((x) * (x))
 int main()
 {
  int y = 5;
- int z = SQR(y + 1);
+ int z = 1 / SQR(y + 1);
  printf("z = %d\n", z);
  return 0;
 }   
