cca79f3787350f4b6b7838ab95d03cbdb7221cb2
Дмитрюк Вячеслав Вячеславович <is241s06@bwp15.csc.local>
2023-03-31 13:15:28 +0700

    classwork

--- lab03/task2/coder.h
+++ lab03/task2/coder.h
@@ -1,11 +1,4 @@
-enum {
-    MaxCodeLength = 4
-};
 
-typedef struct {
-    uint8_t code[MaxCodeLength];
-    size_t length;
-} CodeUnits;
 
 int encode(uint32_t code_point, CodeUnits *code_units);
 uint32_t decode(const CodeUnits *code_units);
